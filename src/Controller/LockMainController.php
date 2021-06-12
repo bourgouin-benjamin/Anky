1cae82b2fb25cbc5b296b096b66a2d5c4813fac4
Benjamin Bourgouin <67418255+KayleoRakeon@users.noreply.github.com>
2021-06-13 00:55:21 +0200

    Ajout des étapes

--- src/Controller/LockMainController.php
+++ src/Controller/LockMainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 use App\Entity\LockMain;
+use App\Form\LockMainType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,24 @@ class LockMainController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $lockMain = new LockMain();
+        $form = $this->createForm(LockMainType::class, $lockMain);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($lockMain);
+            $em->flush();
+        }
+
+        $lockMain = $em->getRepository(LockMain::class)->findAll();
+
         return $this->render('lock_main/index.html.twig', [
-            'controller_name' => 'LockMainController',
+            'lockMain' => $lockMain,
+            'ajout' => $form->createView(),
         ]);
     }
 }
