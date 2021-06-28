13b096e6413e80abda68b020b202759989467f03
Benjamin Bourgouin <67418255+KayleoRakeon@users.noreply.github.com>
2021-06-29 00:57:38 +0200

    Ajout Catégorie + série

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -3,6 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Form\CategorieType;
+use App\Entity\Genre;
+use App\Form\GenreType;
+
+use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +18,41 @@ class SeriesController extends AbstractController
     /**
      * @Route("/series", name="series")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $em = $this->getDoctrine()->getManager();
 
+
+        // CATEGORIE
+        $categorie = new Categorie();
+        $formCat = $this->createForm(CategorieType::class, $categorie);
+        $formCat->handleRequest($request);
+
+        if($formCat->isSubmitted() && $formCat->isValid()){
+            $em->persist($categorie);
+            $em->flush();
+        }
+
         $categorie = $em->getRepository(Categorie::class)->findAll();
+
+
+        // GENRE
+        $genre = new Genre();
+        $formGen = $this->createForm(GenreType::class, $genre);
+        $formGen->handleRequest($request);
+
+        if($formGen->isSubmitted() && $formGen->isValid()){
+            $em->persist($genre);
+            $em->flush();
+        }
+
+        $genre = $em->getRepository(Genre::class)->findAll();
 
         return $this->render('series/index.html.twig', [
             'categorie' => $categorie,
+            'ajoutCategorie' => $formCat->createView(),
+            'genre' => $genre,
+            'ajoutGenre' => $formGen->createView(),
         ]);
     }
 }
