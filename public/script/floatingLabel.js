ab3012db7241754783daba70ac483232f5c9a01b
Benjamin Bourgouin <67418255+KayleoRakeon@users.noreply.github.com>
2021-07-27 01:40:33 +0200

    Log in done

--- public/script/floatingLabel.js
+++ public/script/floatingLabel.js
@@ -1,8 +1,6 @@
 let label = document.querySelectorAll('.register label');
 let input = document.querySelectorAll('.input');
 
-
-
 for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("focus", () => {
         label[i].classList.add('floatingLabel');
@@ -11,5 +9,19 @@ for (let i = 0; i < input.length; i++) {
 
     input[i].addEventListener("blur", () => {
         label[i].classList.remove('fontColor');
+    })
+}
+
+
+let labelLogin = document.querySelectorAll('.login label');
+
+for (let i = 0; i < input.length; i++) {
+    input[i].addEventListener("focus", () => {
+        labelLogin[i].classList.add('floatingLabel');
+        labelLogin[i].classList.add('fontColor');
+    })
+
+    input[i].addEventListener("blur", () => {
+        labelLogin[i].classList.remove('fontColor');
     })
 }
